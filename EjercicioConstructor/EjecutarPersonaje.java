5024c4733c59bb1a8a6d78da5142499fa72c0058
Wilmer1704 <wilmerpoche25@hotmail.com>
2026-03-04 01:35:18 +0000

    Personaje

---
+++ EjercicioConstructor/EjecutarPersonaje.java
@@ -0,0 +1,11 @@
+public class ejecutarPersonaje {
+    public static void main(String[] args) {
+        // Crear un personaje usando el constructor
+        Personaje p1 = new Personaje("Superman");
+         p1.mostrarInfo();
+
+    
+    
+    }
+    
+}
